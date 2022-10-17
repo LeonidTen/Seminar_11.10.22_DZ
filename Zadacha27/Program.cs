ff6bbcacfc1b6c70d08f04901ec6f7a1b801f2a6
leonid <tenleonid@gmail.com>
2022-10-17 09:14:03 +0600

    Dz27

---
+++ Zadacha27/Program.cs
@@ -0,0 +1,20 @@
+﻿/* Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+	
+	452 -> 11
+	
+	82 -> 10
+	
+	9012 -> 12
+	*/
+	
+
+	Console.WriteLine("Введите число: ");
+	int num = int.Parse(Console.ReadLine());
+	int sum = 0;
+	while(num > 0)
+	{
+	    sum = sum + num % 10;
+	    num = num / 10;
+	}
+	Console.WriteLine($"Сумма числа равна: {sum}");
+
